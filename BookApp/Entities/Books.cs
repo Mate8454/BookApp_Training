9fdc2549e3bb6c8ba760c4eda4fb0bb6f8e10349
Satyam19102002 <Satyamsherawat19102002@gmail.com>
2024-12-11 17:19:50 +0530

    UpdatedBookEntity

--- BookApp/Entities/Books.cs
+++ BookApp/Entities/Books.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,32 @@ namespace BookApp.Entities
 {
     public class Books
     {
+        [Required]
+        [Key]
+        public int BookId { get; set; }
+
+        [Required]
+        public string BookTitile { get; set; }
+        [Required]
+        public string Author { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public int BookQuantity { get; set; }
+
+        [Required]
+        public string BookCover { get; set; }
+
+        [Required]
+        
+        public int PublishedYear { get; set; }
     }
 }
