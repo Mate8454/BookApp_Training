de82d4e5820ea4770360f4436721cf1b73d5c61f
jivan-ranjeet-mate <jivan-ranjeet.mate@novelvistanet.com>
2024-12-11 17:23:12 +0530

    cartEntityadded

---
+++ BookApp/Entities/Cart.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookApp.Entities
+{
+    public class Cart
+    {
+        [Key]
+        [Required]
+        public int CartId {  get; set; }
+        [Required]
+        [ForeignKey(nameof(Books))]
+        public int BookId { get; set; }
+        [Required]
+        public string BookTitle { get; set; }
+        [Required]
+        [ForeignKey(nameof(User))]
+        public int UserId { get; set; }
+        [Required]
+        public double TotalPrice { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+
+    }
+}
