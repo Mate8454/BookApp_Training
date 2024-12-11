430e48bb15d9f1f71a535db837c1153f617fd337
jivan-ranjeet-mate <jivan-ranjeet.mate@novelvistanet.com>
2024-12-11 16:08:16 +0530

    commit1

---
+++ BookApp/BookApp/Entity/Book.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BookApp.Entity
+{
+    public class Book
+    {
+        [Required]
+        [Key]
+        public int BookId { get; set; }
+        [Required]
+        public string BookName { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public double Price { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime PublishYear { get; set; }
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string CoverImage { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+
+
+    }
+}
