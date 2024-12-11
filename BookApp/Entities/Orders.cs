b7333d772d4ef835c2a98b2fcc9a2c0eef89841f
Manish1234 <manish.negi2002@gmail.com>
2024-12-11 17:55:44 +0530

    OrderEntityAdded

---
+++ BookApp/Entities/Orders.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BookApp.Entities
+{
+    public class Orders
+    {
+        [Required]
+        public int OrderId {  get; set; }
+        [Required]
+        public int UserId {  get; set; }
+        [Required]
+        public DateTime OrderDate { get; set; }
+        [Required]
+        public double TotalAmount {  get; set; }
+        [Required]
+        public string Status {  get; set; }
+        [Required]
+        public string DeliveryAddress {  get; set; }
+        
+
+    }
+}
