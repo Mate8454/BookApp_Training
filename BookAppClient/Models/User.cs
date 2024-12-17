0c600293bfbe6835d9e2324dd0551512c8a7a862
jivan-ranjeet-mate <jivan-ranjeet.mate@novelvistanet.com>
2024-12-17 16:03:57 +0530

    editsomefrontendinbookandpayment

--- BookAppClient/Models/User.cs
+++ BookAppClient/Models/User.cs
@@ -13,6 +13,7 @@ namespace BookAppClient.Models
 
         public string Name { get; set; }
         public string Email { get; set; }
+        
         public string Password { get; set; }
         public string Role { get; set; }
     }
